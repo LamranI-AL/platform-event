483c272e0544d4c8c8eccc584b5a5f60a7965923
othmane LAMRANI ALAOUI <lamraniotman000@gmail.com>
2024-05-01 13:24:22 +0100

    chnage sign-in hero

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -16,32 +16,13 @@ export default function Page() {
 
           <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
             <div className="max-w-xl lg:max-w-3xl">
-              <Link className="block text-blue-600" href="#">
+              <Link className="block text-blue-600" href="/#">
                 <span className="sr-only">Home</span>
-                <svg
-                  id="logo-72"
-                  width="52"
-                  height="44"
-                  viewBox="0 0 53 44"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M23.2997 0L52.0461 28.6301V44H38.6311V34.1553L17.7522 13.3607L13.415 13.3607L13.415 44H0L0 0L23.2997 0ZM38.6311 15.2694V0L52.0461 0V15.2694L38.6311 15.2694Z"
-                    class="ccustom"
-                    fill="#212326"
-                  ></path>
-                </svg>
               </Link>
 
               <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-                Welcome to Squid 🦑
+                Welcome to Quark events 🦑
               </h1>
-
-              {/* <p className="mt-4 leading-relaxed text-gray-500">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Eligendi nam dolorum aliquam, quibusdam aperiam voluptatum.
-              </p> */}
 
               <SignIn />
             </div>
